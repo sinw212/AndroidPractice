108092ad99777c5ed258f1d79501f54afe2b5d77
sinw212 <sinw212@gmail.com>
2024-05-24 23:54:10 +0900

    Data Binding in Android

--- AndroidCodelabs/DataBinding/README.md
+++ AndroidCodelabs/DataBinding/README.md
@@ -1,8 +1,10 @@
-Android Data Binding Codelab
-=============================================
-Follow the codelab in
+# Android Data Binding Codelab
 
-https://github.com/googlecodelabs/android-databinding
+Codelab Link : https://developer.android.com/codelabs/android-databinding
+
+
+### 구현 화면
+<img src="https://github.com/sinw212/KotlinPractice/assets/53486320/8ce82c7c-74fb-449a-9e3e-b1a1f26bbca3" width="600" height="400"/>
 
 
 License
--- AndroidCodelabs/DataBinding/app/src/main/java/com/example/android/databinding/basicsample/data/SimpleViewModel.kt
+++ AndroidCodelabs/DataBinding/app/src/main/java/com/example/android/databinding/basicsample/data/SimpleViewModel.kt
@@ -16,35 +16,34 @@
 
 package com.example.android.databinding.basicsample.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-/**
- * A simple VM for [com.example.android.databinding.basicsample.ui.PlainOldActivity].
- */
 class SimpleViewModel : ViewModel() {
-    val name = "Grace"
-    val lastName = "Hopper"
-    var likes = 0
-        private set // This is to prevent external modification of the variable.
+    private val _name = MutableLiveData("Ada")
+    val name: LiveData<String> = _name
 
-    /**
-     * Increments the number of likes.
-     */
-    fun onLike() {
-        likes++
-    }
+    private val _lastName = MutableLiveData("Lovelace")
+    val lastName: LiveData<String> = _lastName
 
-    /**
-     * Returns popularity in buckets: [Popularity.NORMAL], [Popularity.POPULAR] or [Popularity.STAR]
-     */
-    val popularity: Popularity
-        get() {
-            return when {
-                likes > 9 -> Popularity.STAR
-                likes > 4 -> Popularity.POPULAR
+    private val _likes =  MutableLiveData(0)
+    val likes: LiveData<Int> = _likes
+
+    val popularity = MediatorLiveData<Popularity>().apply {
+        addSource(_likes) {
+            value = when {
+                it > 9 -> Popularity.STAR
+                it > 4 -> Popularity.POPULAR
                 else -> Popularity.NORMAL
             }
         }
+    }
+
+    fun onLike() {
+        _likes.value = (_likes.value ?: 0) + 1
+    }
 }
 
 enum class Popularity {
